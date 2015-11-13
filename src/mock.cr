ede8a587e07b1cf0f3a5e48df472c9a6c9895089
Piotr Szotkowski <chastell@chastell.net>
2015-11-13 09:49:30 +0000

    Move double under Spec::DSL

--- src/mock.cr
+++ src/mock.cr
@@ -21,11 +21,11 @@ module Mock
   end
 end
 
-def double(*args)
-  Mock::Double.new(*args)
-end
-
 module Spec::DSL
+  def double(*args)
+    Mock::Double.new(*args)
+  end
+
   def it(description, file = __FILE__, line = __LINE__)
     Mock.reset
     previous_def
